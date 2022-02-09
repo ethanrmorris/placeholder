59a46b089e45fc62f53786501cdd7814746cc1cc
Ethan Morris <ethan.morris33@gmail.com>
2022-02-08 20:29:32 -0600

    everything

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div>
+    <p>Home of the</p>
+    <h1>Alliance of Sunday Morning Chugs</h1>
+		<img src="asmc.webp" alt="Alliance of Sunday Morning Chugs" />
+		<h2>Sorry, our site is currently under construction.</h2>
+		<h4>Check back soon to see our new site.</h4>
+		<p>
+			If you're looking for live stats, you can find them <a
+				href="https://docs.google.com/spreadsheets/d/1P0A5B1DDZbR7jWB83w-RIAAKD-mYscfuwcjQUkiHL3M"
+				>in this document.</a
+			>
+		</p>
+	</div>
+  <p>Thanks.</p>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		background-color: #212121;
+		color: white;
+		text-align: center;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	img {
+		max-width: 250px;
+		height: auto;
+		margin: 2rem;
+	}
+
+	a {
+		color: white;
+	}
+</style>
